fd8ca0ff76616beee6d9c9d23d6cb32c56558bcb
AnastasiaGOIT <hudzevych.music@gmail.com>
2024-02-19 07:50:31 +0200

    done

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -12,7 +12,7 @@ router.post(
   validateBody(schemasUser.registerSchema),
   ctrl.register
 );
-router.get("/verify/:verificationToken", ctrl.verifyEmail);
+router.get("/verify/:√verificationToken", ctrl.verifyEmail);
 router.post(
   "/verify",
   validateBody(schemasUser.emailSchema),
